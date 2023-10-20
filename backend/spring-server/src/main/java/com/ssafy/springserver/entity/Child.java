114193891e7d72dbd4cf2d40090b866de50267d2
인영교 <yeongkyo1997@gmail.com>
2023-10-20 15:37:57 +0900

    Feat: child entity 작성 - child_id : UUID - name : VARCHAR(20) - level : INTEGER - experience : INTEGER

---
+++ backend/spring-server/src/main/java/com/ssafy/springserver/entity/Child.java
@@ -0,0 +1,38 @@
+package com.ssafy.springserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "child")
+public class Child {
+    @Id
+    @Column(name = "child_id", nullable = false)
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(columnDefinition = "default 0")
+    private Integer experience;
+
+    @PrePersist
+    private void prePersist() {
+        this.experience = 0;
+    }
+}
