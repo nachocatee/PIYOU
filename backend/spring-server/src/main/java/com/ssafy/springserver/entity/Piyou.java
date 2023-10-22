d8e6d47dbddf97488e915630889da6dd51c7f71a
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-10-22 15:11:19 +0900

    Feat : Piyou entity 작성 - 이름 - 파일 경로

---
+++ backend/spring-server/src/main/java/com/ssafy/springserver/entity/Piyou.java
@@ -0,0 +1,23 @@
+package com.ssafy.springserver.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "piyou")
+public class Piyou {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "piyou_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 330)
+    private String path;
+}
