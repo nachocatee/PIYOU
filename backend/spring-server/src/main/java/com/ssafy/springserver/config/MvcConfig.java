d9f8b371ab642296d6d3423428a7ac1c83e642d3
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-10-29 19:23:46 +0900

    Feat : docker-compose 수정

---
+++ backend/spring-server/src/main/java/com/ssafy/springserver/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.ssafy.springserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    // cors 허용
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/api")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*");
+    }
+}
