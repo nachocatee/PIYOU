1dcd13e6f25ddd79bd4ecc62fec8e2604ffd1d57
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-10-29 18:51:53 +0900

    Feat : docker-compose 수정

--- backend/spring-server/src/main/java/com/ssafy/springserver/repository/CollectedRepository.java
+++ backend/spring-server/src/main/java/com/ssafy/springserver/repository/CollectedRepository.java
@@ -1,6 +1,5 @@
 package com.ssafy.springserver.repository;
 
-import com.ssafy.springserver.entity.Child;
 import com.ssafy.springserver.entity.Collected;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +7,4 @@ import java.util.List;
 import java.util.UUID;
 public interface CollectedRepository extends JpaRepository<Collected, Long> {
     List<Collected> findByChildId(UUID id);
-    List<Collected> findByChild(Child child);
 }
