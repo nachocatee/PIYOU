a43a74b39431e60ef8459a2ac922fd2ab3819abe
인영교 <yeongkyo1997@gmail.com>
2023-11-02 14:58:35 +0900

    Feat : merge piyou web site

--- piyou-info/src/components/main/MainView.vue
+++ piyou-info/src/components/main/MainView.vue
@@ -2,28 +2,31 @@
 </script>
 
 <template>
-  <div class="col-12 mb20 pt10 pb28 col-center mw-300 tc main_title">피유</div>
-  <div class="col-12 tc mb30 main_desc">아이들의 식사 메이트<br/>피유</div>
-  <div class="col-12 mb50">
-    <img src="@/assets/resource/img/main_contents_img.png" alt="피유 메인 이미지"/>
-  </div>
-  <div class="col-12 col-center mw-227">
-    <div class="col-12 main_btn_con">
-      <div class="col-6 main_btn">
-        <a href="https://piyou.notion.site/PIYOU-7f1b3b65db654aaa8c0171e807208c59?pvs=4" class="col-12 main_btn_inner">
-          패치노트
-        </a>
-      </div>
-      <div class="col-6 main_btn">
-        <a href="https://forms.gle/C6ZvDbZGL3E7joJo6" target="_blank" class="col-12 main_btn_inner">
-          건의사항
-        </a>
-      </div>
-      <div class="col-12 mt15 main_btn type_2">
-        <a href="javascirpt:void(0)" class="col-12 main_btn_inner">
-          다운로드
-        </a>
+  <div class="col-12">
+    <div class="col-12 mb15 pt10 pb28 col-center mw-300 tc main_title">피유</div>
+    <div class="col-12 tc mb20 main_desc">아이들의 식사 메이트<br/>피유</div>
+    <div class="col-12 col-center mw-400 mb30">
+      <img src="@/assets/resource/img/main_contents_img.png" alt="피유 메인 이미지"/>
+    </div>
+    <div class="col-12 col-center mw-227">
+      <div class="col-12 main_btn_con">
+        <div class="col-6 main_btn">
+          <a href="https://piyou.notion.site/PIYOU-7f1b3b65db654aaa8c0171e807208c59?pvs=4" class="col-12 main_btn_inner">
+            패치노트
+          </a>
+        </div>
+        <div class="col-6 main_btn">
+          <a href="https://forms.gle/C6ZvDbZGL3E7joJo6" target="_blank" class="col-12 main_btn_inner">
+            건의사항
+          </a>
+        </div>
+        <div class="col-12 mt10 main_btn type_2">
+          <a href="javascirpt:void(0)" class="col-12 main_btn_inner">
+            다운로드
+          </a>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
