54f6d611f1abca796c1c8bd5cd28d0d8cf6bff40
인영교 <yeongkyo1997@gmail.com>
2023-11-03 10:02:57 +0900

    Fix formatting in MainView.vue

--- piyou-info/src/components/main/MainView.vue
+++ piyou-info/src/components/main/MainView.vue
@@ -4,9 +4,9 @@
 <template>
   <div class="col-12">
     <div class="col-12 mb15 pt10 pb28 col-center mw-300 tc main_title">피유</div>
-    <div class="col-12 tc mb20 main_desc">아이들의 식사 메이트<br/>피유</div>
+    <div class="col-12 tc mb20 main_desc">아이들의 식사 메이트<br />피유</div>
     <div class="col-12 col-center mw-400 mb30">
-      <img src="@/assets/resource/img/main_contents_img.png" alt="피유 메인 이미지"/>
+      <img src="@/assets/resource/img/main_contents_img.png" alt="피유 메인 이미지" />
     </div>
     <div class="col-12 col-center mw-227">
       <div class="col-12 main_btn_con">
@@ -21,7 +21,8 @@
           </a>
         </div>
         <div class="col-12 mt10 main_btn type_2">
-          <a href="javascirpt:void(0)" class="col-12 main_btn_inner">
+          <a href="https://m.onestore.co.kr/mobilepoc/apps/appsDetail.omp?prodId=0000772452"
+            class="col-12 main_btn_inner">
             다운로드
           </a>
         </div>
