c27d4e5e0fc53a1e1e21b04ee3fc1b01c9b299ee
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-11-07 20:30:00 +0900

    Add findByEngName method to PiyouRepository

--- backend/spring-server/src/main/java/com/ssafy/springserver/repository/PiyouRepository.java
+++ backend/spring-server/src/main/java/com/ssafy/springserver/repository/PiyouRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.springserver.entity.Piyou;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+
 public interface PiyouRepository extends JpaRepository<Piyou, Long> {
     Optional<Piyou> findByEngName(String engName);
 }
