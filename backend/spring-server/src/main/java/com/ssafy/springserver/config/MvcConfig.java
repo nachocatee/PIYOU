9d19a788a89b0931c1dd20ed915dd6f0041ea485
인영교 <yeongkyo1997@gmail.com>
2023-11-07 14:14:13 +0900

    Add CORS configuration to MvcConfig

--- backend/spring-server/src/main/java/com/ssafy/springserver/config/MvcConfig.java
+++ backend/spring-server/src/main/java/com/ssafy/springserver/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.springserver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     // cors 허용
