1a5e0e680c407e055c062da50d17875df84b39e4
인영교 <yeongkyo1997@gmail.com>
2023-11-08 10:50:11 +0900

    Add Collected entity and transactional annotation to ChildService

--- backend/spring-server/src/main/java/com/ssafy/springserver/service/ChildService.java
+++ backend/spring-server/src/main/java/com/ssafy/springserver/service/ChildService.java
@@ -4,13 +4,16 @@ import com.ssafy.springserver.dto.ChildRequest;
 import com.ssafy.springserver.dto.ChildResponse;
 import com.ssafy.springserver.dto.StatusResponse;
 import com.ssafy.springserver.entity.Child;
+import com.ssafy.springserver.entity.Collected;
 import com.ssafy.springserver.entity.Piyou;
 import com.ssafy.springserver.entity.Status;
 import com.ssafy.springserver.repository.ChildRepository;
+import com.ssafy.springserver.repository.CollectedRepository;
 import com.ssafy.springserver.repository.PiyouRepository;
 import com.ssafy.springserver.repository.StatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -21,12 +24,21 @@ public class ChildService {
     private final ChildRepository childRepository;
     private final StatusRepository statusRepository;
     private final PiyouRepository piyouRepository;
+    private final CollectedRepository collectedRepository;
 
+    @Transactional
     public ChildResponse createChild(ChildRequest childRequest) {
         Status status = statusRepository.save(new Status());
         Child child = childRepository.save(Child.builder().name(childRequest.getName()).status(status).build());
-        Piyou piyou = piyouRepository.findById(child.getStatus().getPiyouId())
+        Piyou piyou = piyouRepository.findById(1L)
                 .orElseThrow(() -> new IllegalArgumentException("해당 피유가 없습니다."));
+
+        collectedRepository.save(
+                Collected.builder()
+                        .child(child)
+                        .piyou(piyou)
+                        .build()
+        );
 
         return ChildResponse.fromEntity(child, StatusResponse.fromEntity(status, piyou.getEngName()));
     }
@@ -39,6 +51,7 @@ public class ChildService {
         return ChildResponse.fromEntity(child, StatusResponse.fromEntity(child.getStatus(), piyou.getEngName()));
     }
 
+    @Transactional
     public ChildResponse updateChild(UUID childId, ChildRequest child) {
         Child childEntity = childRepository.findById(childId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 아이가 없습니다."));
