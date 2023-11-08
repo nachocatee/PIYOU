09c269286a6966740f8b9749b76cb90ab275727a
인영교 <yeongkyo1997@gmail.com>
2023-11-08 10:22:56 +0900

    Add endpoints for getting and creating hats

--- backend/spring-server/src/main/java/com/ssafy/springserver/controller/HatController.java
+++ backend/spring-server/src/main/java/com/ssafy/springserver/controller/HatController.java
@@ -1,21 +1,38 @@
 package com.ssafy.springserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.Getter;
+import com.ssafy.springserver.service.HatService;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/hat")
 public class HatController {
-    @GetMapping(value = "/getMethodName")
-    public ResponseEntity<?> getHat() {
-        return ResponseEntity.ok();
+    private final HatService hatService;
+
+    /**
+     * 아이가 수집한 모자 정보 조회
+     *
+     * @param childId 아이 식별자
+     * @return 모자 정보
+     */
+    @GetMapping("/{childId}")
+    public ResponseEntity<?> getHat(@PathVariable UUID childId) {
+        return ResponseEntity.ok(hatService.getHat(childId));
+    }
+
+    /**
+     * 아이 모자 등록
+     *
+     * @param childId 아이 식별자
+     * @param hatId   모자 식별자
+     * @return 모자 정보
+     */
+    @PostMapping("/{childId}/{hatId}")
+    public ResponseEntity<?> createHat(@PathVariable UUID childId, @PathVariable Long hatId) {
+        return ResponseEntity.ok(hatService.createHat(childId, hatId));
     }
 }
