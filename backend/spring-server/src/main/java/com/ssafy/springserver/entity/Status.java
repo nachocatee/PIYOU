279bac79e8b4322fdafdcaffba7a3a7b0ca964ff
인영교 <yeongkyo1997@gmail.com>
2023-11-08 16:13:43 +0900

    Remove unused fields and methods in Status entity.

--- backend/spring-server/src/main/java/com/ssafy/springserver/entity/Status.java
+++ backend/spring-server/src/main/java/com/ssafy/springserver/entity/Status.java
@@ -19,22 +19,8 @@ public class Status {
     @Column(name = "piyou_id")
     private Long piyouId;
 
-    @Column(columnDefinition = "INT DEFAULT 0")
-    private Integer hungry;
-
-    @Column(columnDefinition = "INT DEFAULT 0")
-    private Integer health;
-    private Boolean death;
-
-    public void piyouDeath() {
-        this.death = true;
-    }
-
     @PrePersist
     private void prePersist() {
         this.piyouId = 1L;
-        this.hungry = 0;
-        this.health = 0;
-        this.death = false;
     }
 }
