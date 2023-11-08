ba375de292549f1676ea40db041a23a781a978c2
인영교 <yeongkyo1997@gmail.com>
2023-11-08 13:07:20 +0900

    Add findByName method to HatRepository

--- backend/spring-server/src/main/java/com/ssafy/springserver/repository/HatRepository.java
+++ backend/spring-server/src/main/java/com/ssafy/springserver/repository/HatRepository.java
@@ -2,5 +2,8 @@ package com.ssafy.springserver.repository;
 
 import com.ssafy.springserver.entity.Hat;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 public interface HatRepository extends JpaRepository<Hat, Long> {
+    Optional<Hat> findByName(String name);
 }
