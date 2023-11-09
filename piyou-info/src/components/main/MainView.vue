bac6b3a7573b2131ab72c42f703bf47dfd2fed03
정민 <k3371548@naver.com>
2023-11-09 20:50:09 +0900

    [STYLE] 1109 piyou-info commit

--- piyou-info/src/components/main/MainView.vue
+++ piyou-info/src/components/main/MainView.vue
@@ -9,7 +9,7 @@
       <img src="@/assets/resource/img/main_contents_img.png" alt="피유 메인 이미지"/>
     </div>
     <div class="col-12 col-center mw-227">
-      <div class="col-12 main_btn_con">
+      <div class="col-12 mb20 main_btn_con">
         <div class="col-12 main_btn type_2">
           <a href="https://play.google.com/store/apps/details?id=com.b106_402jeoung.PIYOU" class="col-12 main_btn_inner">
             <img src="@/assets/resource/img/google_play_icon.png" alt="구글 플레이 스토어 아이콘" style="width: 32px;"/>
@@ -33,6 +33,23 @@
           </a>
         </div>
       </div>
+      <ul class="col-12 info_list">
+          <li>
+            <a href="https://instagram.com/piyou_app?utm_source=qr">
+              <img src="@/assets/resource/img/instagram_icon.png" alt="인스타그램 아이콘"/>
+            </a>
+          </li>
+          <li>
+            <a href="http://pf.kakao.com/_xfIysG">
+              <img src="@/assets/resource/img/kakao_icon.png" alt="카카오톡 아이콘"/>
+            </a>
+          </li>
+          <li>
+            <a href="mailto:piyou.developers@gmail.com">
+              <img src="@/assets/resource/img/gmail_icon.png" alt="지메일 아이콘"/>
+            </a>
+          </li>
+        </ul>
     </div>
 
   </div>
