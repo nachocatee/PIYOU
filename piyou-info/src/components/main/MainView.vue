fa868aeff7c1269609f6dbc03451bf84f71d8b5d
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-11-09 21:58:30 +0900

    Add Google Play and One Store download links to main view

--- piyou-info/src/components/main/MainView.vue
+++ piyou-info/src/components/main/MainView.vue
@@ -9,7 +9,20 @@
       <img src="@/assets/resource/img/main_contents_img.png" alt="피유 메인 이미지" />
     </div>
     <div class="col-12 col-center mw-227">
-      <div class="col-12 main_btn_con">
+      <div class="col-12 mb20 main_btn_con">
+        <div class="col-12 main_btn type_2">
+          <a href="https://play.google.com/store/apps/details?id=com.b106_402jeoung.PIYOU" class="col-12 main_btn_inner">
+            <img src="@/assets/resource/img/google_play_icon.png" alt="구글 플레이 스토어 아이콘" style="width: 32px;" />
+            <span>Google Play</span>
+          </a>
+        </div>
+        <div class="col-12 main_btn type_2">
+          <a href="https://m.onestore.co.kr/mobilepoc/apps/appsDetail.omp?prodId=0000772452"
+            class="col-12 main_btn_inner">
+            <img src="@/assets/resource/img/one_store_icon.png" alt="원스토어 아이콘" style="width: 32px;" />
+            <span>One Store</span>
+          </a>
+        </div>
         <div class="col-6 main_btn">
           <a href="https://piyou.notion.site/PIYOU-7f1b3b65db654aaa8c0171e807208c59?pvs=4" class="col-12 main_btn_inner">
             패치노트
@@ -20,30 +33,24 @@
             건의사항
           </a>
         </div>
-        <div class="col-12 mt10 main_btn type_2">
-          <a href="https://m.onestore.co.kr/mobilepoc/apps/appsDetail.omp?prodId=0000772452"
-            class="col-12 main_btn_inner">
-            다운로드
-          </a>
-        </div>
       </div>
       <ul class="col-12 info_list">
-          <li>
-            <a href="https://instagram.com/piyou_app?utm_source=qr">
-              <img src="@/assets/resource/img/instagram_icon.png" alt="인스타그램 아이콘"/>
-            </a>
-          </li>
-          <li>
-            <a href="http://pf.kakao.com/_xfIysG">
-              <img src="@/assets/resource/img/kakao_icon.png" alt="카카오톡 아이콘"/>
-            </a>
-          </li>
-          <li>
-            <a href="mailto:piyou.developers@gmail.com">
-              <img src="@/assets/resource/img/gmail_icon.png" alt="지메일 아이콘"/>
-            </a>
-          </li>
-        </ul>
+        <li>
+          <a href="https://instagram.com/piyou_app?utm_source=qr">
+            <img src="@/assets/resource/img/instagram_icon.png" alt="인스타그램 아이콘" />
+          </a>
+        </li>
+        <li>
+          <a href="http://pf.kakao.com/_xfIysG">
+            <img src="@/assets/resource/img/kakao_icon.png" alt="카카오톡 아이콘" />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:piyou.developers@gmail.com">
+            <img src="@/assets/resource/img/gmail_icon.png" alt="지메일 아이콘" />
+          </a>
+        </li>
+      </ul>
     </div>
 
   </div>
