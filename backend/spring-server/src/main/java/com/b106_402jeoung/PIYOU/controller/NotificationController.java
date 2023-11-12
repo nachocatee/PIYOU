f6c04cb496cf080bc4c5d74e6de2fe83f909ce28
인영교 <yeongkyo1997@gmail.com>
2023-11-12 12:16:00 +0900

    Refactor NotificationController to use NotificationScheduler

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/NotificationController.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/NotificationController.java
@@ -1,30 +1,19 @@
 package com.b106_402jeoung.PIYOU.controller;
 
-import com.b106_402jeoung.PIYOU.dto.NotificationRequest;
-import com.b106_402jeoung.PIYOU.dto.NotificationResponse;
-import com.b106_402jeoung.PIYOU.service.FCMService;
+import com.b106_402jeoung.PIYOU.service.NotificationScheduler;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/noti")
 public class NotificationController {
-    private final FCMService fcmService;
+    private final NotificationScheduler notificationScheduler;
 
-    @PostMapping("")
-    public ResponseEntity<?> sendNotification(@RequestBody NotificationRequest request)
-            throws ExecutionException, InterruptedException {
-        fcmService.sendMessageToToken(request);
-        return new ResponseEntity<>(NotificationResponse.builder()
-                                            .status(HttpStatus.OK.value())
-                                            .message("Notification has been sent."), HttpStatus.OK);
+    @RequestMapping("/test")
+    public void test() throws FirebaseMessagingException {
+        notificationScheduler.pushMorningMealAlarm();
     }
 }
