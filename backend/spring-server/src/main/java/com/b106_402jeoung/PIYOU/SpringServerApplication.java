9628265794a1f2366bebf5970963f1de1144616a
인영교 <yeongkyo1997@gmail.com>
2023-11-12 12:16:48 +0900

    Remove scheduling annotation from SpringServerApplication

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/SpringServerApplication.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/SpringServerApplication.java
@@ -2,10 +2,8 @@ package com.b106_402jeoung.PIYOU;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class SpringServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringServerApplication.class, args);
