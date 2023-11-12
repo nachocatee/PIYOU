5cfb9cf897bad17162ad404522949d893f8d6b39
인영교 <yeongkyo1997@gmail.com>
2023-11-12 15:44:51 +0900

    Commented out NotificationController code.

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/NotificationController.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/NotificationController.java
@@ -1,19 +1,19 @@
-package com.b106_402jeoung.PIYOU.controller;
-
-import com.b106_402jeoung.PIYOU.service.NotificationScheduler;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/noti")
-public class NotificationController {
-    private final NotificationScheduler notificationScheduler;
-
-    @RequestMapping("/test")
-    public void test() throws FirebaseMessagingException {
-        notificationScheduler.pushMorningMealAlarm();
-    }
-}
+//package com.b106_402jeoung.PIYOU.controller;
+//
+//import com.b106_402jeoung.PIYOU.service.NotificationScheduler;
+//import com.google.firebase.messaging.FirebaseMessagingException;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/noti")
+//public class NotificationController {
+//    private final NotificationScheduler notificationScheduler;
+//
+//    @RequestMapping("/test")
+//    public void test() throws FirebaseMessagingException {
+//        notificationScheduler.pushMorningMealAlarm();
+//    }
+//}
