777a55acdf63940c06bc176b7ec068af7c0b71e3
인영교 <yeongkyo1997@gmail.com>
2023-11-13 17:37:59 +0900

    Add ChildNoti entity for notifications

---
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/entity/ChildNoti.java
@@ -0,0 +1,28 @@
+package com.b106_402jeoung.PIYOU.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChildNoti {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "child_noti_id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "child_id")
+    private Child child;
+
+    @ManyToOne
+    @JoinColumn(name = "notification_id")
+    private Notification notification;
+}
