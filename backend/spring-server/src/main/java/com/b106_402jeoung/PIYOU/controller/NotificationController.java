d3f9404b5e05bd04f5a2195c6c45eb0056a371d5
인영교 <yeongkyo1997@gmail.com>
2023-11-14 15:58:18 +0900

    Add endpoints for push notifications

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/NotificationController.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/NotificationController.java
@@ -1,12 +1,13 @@
 package com.b106_402jeoung.PIYOU.controller;
 
+import com.b106_402jeoung.PIYOU.dto.ChildNotiRequest;
 import com.b106_402jeoung.PIYOU.dto.PushRequest;
 import com.b106_402jeoung.PIYOU.service.PushService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 
 @RestController
@@ -15,11 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
     private final PushService pushService;
 
+    @GetMapping("{childId}")
+    public ResponseEntity<?> getNoti(@PathVariable UUID childId) {
+        return ResponseEntity.ok(pushService.getPush(childId));
+    }
 
-    @PostMapping("/register")
-    public String registerToken(@RequestBody PushRequest pushRequest) {
-        pushService.sendPush(pushRequest);
+    @PostMapping("/send")
+    public String sendPush(@RequestBody PushRequest pushRequest, @RequestBody ChildNotiRequest childNotiRequest) {
+        pushService.sendPush(pushRequest, childNotiRequest);
 
         return "등록완료";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerPush(@RequestBody ChildNotiRequest childNotiRequest) {
+        return ResponseEntity.ok(pushService.registerPush(childNotiRequest));
+    }
+
+    @DeleteMapping("/{notiId}")
+    public String deletePush(@PathVariable Long notiId) {
+        pushService.deletePush(notiId);
+        return "삭제되었습니다.";
     }
 }
