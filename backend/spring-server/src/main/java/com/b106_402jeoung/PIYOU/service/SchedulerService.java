25543f370a514eb9f4fe07ff7e5c9033fc1dfb75
인영교 <yeongkyo1997@gmail.com>
2023-11-15 16:48:18 +0900

    Add ChildNotiRepository and PushService to SchedulerService for push notification

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/service/SchedulerService.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/service/SchedulerService.java
@@ -1,6 +1,9 @@
 package com.b106_402jeoung.PIYOU.service;
 
+import com.b106_402jeoung.PIYOU.dto.PushRequest;
 import com.b106_402jeoung.PIYOU.entity.Child;
+import com.b106_402jeoung.PIYOU.entity.ChildNoti;
+import com.b106_402jeoung.PIYOU.repository.ChildNotiRepository;
 import com.b106_402jeoung.PIYOU.repository.ChildRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +17,8 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class SchedulerService {
+    private final ChildNotiRepository childNotiRepository;
+    private final PushService pushService;
     private final ChildRepository childRepository;
     @PersistenceContext
     private final EntityManager entityManager;
@@ -50,5 +55,18 @@ public class SchedulerService {
 
         entityManager.flush();
         entityManager.clear();
+    }
+
+    public void pushTracking() {
+        List<ChildNoti> oldNoti = childNotiRepository.findOldNoti();
+
+        for (ChildNoti childNoti : oldNoti) {
+            pushService.sendPush(PushRequest.builder()
+                                         .title("밥 먹기 10분 전이예요")
+                                         .message("방송 시간을 지켜주세요.\n" + "시청자들과의 약속을 지켜주세요")
+                                         .tokenList(childNoti.getChild()
+                                                            .getToken())
+                                         .build());
+        }
     }
 }
