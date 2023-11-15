4d040afec577a7c1edb6f481557536cc4cf8c4dc
인영교 <yeongkyo1997@gmail.com>
2023-11-15 16:48:09 +0900

    Add query to find old notifications in ChildNotiRepository

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/repository/ChildNotiRepository.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/repository/ChildNotiRepository.java
@@ -2,6 +2,7 @@ package com.b106_402jeoung.PIYOU.repository;
 
 import com.b106_402jeoung.PIYOU.entity.ChildNoti;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,4 +11,8 @@ public interface ChildNotiRepository extends JpaRepository<ChildNoti, Long> {
     void deleteByNotificationId(Long id);
 
     List<ChildNoti> findByChildIdOrderByNotificationTimeAsc(UUID id);
+
+    // 시간이 현재시간보다 10분전인 데이터를 찾는 쿼리
+    @Query(value = "SELECT * FROM child_noti WHERE notification_id IN (SELECT notification_id From notification WHERE TIME_FORMAT(time, '%H:%i') = TIME_FORMAT(CURTIME()-INTERVAL 10 MINUTE, '%H:%i'))", nativeQuery = true)
+    List<ChildNoti> findOldNoti();
 }
