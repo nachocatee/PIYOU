a23346d270d94c95fe2283237845b9149b2d0df8
인영교 <yeongkyo1997@gmail.com>
2023-11-15 17:26:27 +0900

    Fix query to find old notifications

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/repository/ChildNotiRepository.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/repository/ChildNotiRepository.java
@@ -13,6 +13,6 @@ public interface ChildNotiRepository extends JpaRepository<ChildNoti, Long> {
     List<ChildNoti> findByChildIdOrderByNotificationTimeAsc(UUID id);
 
     // 시간이 현재시간보다 10분전인 데이터를 찾는 쿼리
-    @Query(value = "SELECT * FROM child_noti WHERE notification_id IN (SELECT notification_id From notification WHERE TIME_FORMAT(time, '%H:%i') = TIME_FORMAT(CURTIME()-INTERVAL 10 MINUTE, '%H:%i'))", nativeQuery = true)
+    @Query(value = "SELECT * FROM child_noti WHERE notification_id IN (SELECT notification_id From notification WHERE TIME_FORMAT(time, '%H:%i') = TIME_FORMAT(CURTIME() + INTERVAL 10 MINUTE, '%H:%i'))", nativeQuery = true)
     List<ChildNoti> findOldNoti();
 }
