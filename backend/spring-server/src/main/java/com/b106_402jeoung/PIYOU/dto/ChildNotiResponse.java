eb122ca7ccdfd7761de1e48f0f31cf126afd1548
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-11-28 14:12:08 +0900

    Add @Builder annotation to ChildNotiResponse class
    Former-commit-id: 404985c4a67d81f2582d4c79604fd2282a6c20ad

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/dto/ChildNotiResponse.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/dto/ChildNotiResponse.java
@@ -1,12 +1,14 @@
 package com.b106_402jeoung.PIYOU.dto;
 
 import com.b106_402jeoung.PIYOU.entity.ChildNoti;
+import lombok.Builder;
 import lombok.Value;
 
 /**
  * DTO for {@link com.b106_402jeoung.PIYOU.entity.ChildNoti}
  */
 @Value
+@Builder
 public class ChildNotiResponse {
     ChildResponse child;
     NotificationResponse notification;
